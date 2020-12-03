cb964e109e78f36bf60d1b584f809ca03f6e7361
unknown <m.jafarabdi@gmail.com>
2020-12-03 04:39:37 +0330

    timeslot -> 7

--- src/services/time-service.js
+++ src/services/time-service.js
@@ -5,7 +5,7 @@ const dateformat = require('dateformat');
 const {Booking} = require('../models/Booking');
 
 
-const MAX_BOOKING_PER_SLOT = 5;
+const MAX_BOOKING_PER_SLOT = 7;
 
 function getNow()
 {
