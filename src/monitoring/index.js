66ddea51318ce95efe492c591ce575b412c41533
unknown <m.jafarabdi@gmail.com>
2020-12-07 15:48:30 +0330

    monitoring system

--- src/monitoring/index.js
+++ src/monitoring/index.js
@@ -1,13 +1,18 @@
-const TrustedComms = require("twilio/lib/rest/preview/TrustedComms");
+
 const {Notification} = require('./../models/Notification');
 const mongodb = require('./../mongodb');
 const { sendTextMessage } = require("./twilio-sender");
 
-mongodb();
+mongodb().then( () => 
+    {
+        log('waiting for new notifications....');
+    });
+
+
 
 const numbers = [
     '+989126972729',
-    // '+447701007261'
+    '+447701007261'
 ];
 
 const timer = setInterval(() => {
@@ -16,22 +21,33 @@ const timer = setInterval(() => {
 
 async function sendNotifications()
 {
-    const newNotifications = await Notification.find({ sent : {$ne : true}}).exec();
-    if (newNotifications && newNotifications.length > 0)
+    try
     {
-        for (var i = 0; i < newNotifications.length; i++)
+        const newNotifications = await Notification.find({ sent : {$ne : true}}).exec();
+        if (newNotifications && newNotifications.length > 0)
         {
-            const notification = newNotifications[i];
-
-            for (var j = 0; j < numbers.length; j++)
+            for (var i = 0; i < newNotifications.length; i++)
             {
-                sendTextMessage(numbers[j],notification.text);
-                console.log(`${new Date().to}`)
+                const notification = newNotifications[i];
+    
+                for (var j = 0; j < numbers.length; j++)
+                {
+                    sendTextMessage(numbers[j],`You have an Alert from PCR-BOT : \n\n ${notification.text}` );
+                    log(`Message : {"${notification.text}"} sent to ${numbers[j]}`);
+                }
+                
+                await Notification.updateOne({_id: notification._id}, {sent : true, sentTimeStamp: new Date()});
             }
 
+            log('waiting for new notifications....');   
         }
-
+    }catch(err)
+    {
+        log(`Error : ${err}'`);
     }
+}
 
-
+function log(text)
+{
+    console.log(`${new Date().toUTCString()} : ${text}`);
 }
