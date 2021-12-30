7b0bf84dd90e6a7d0fb2381b6d19e24f1928953c
mjabdi <m.jafarabdi@gmail.com>
2021-12-30 16:14:56 +0330

    update cal

--- src/services/holidays.js
+++ src/services/holidays.js
@@ -198,7 +198,7 @@ const getDefaultTimeSlots = (date) =>
     }
 
 
-    const is23Dec = (dateStr === '2021-12-23' || dateStr === '2021-12-27' || dateStr === '2021-12-28' || dateStr === '2021-12-31' )
+    const is23Dec = (dateStr === '2021-12-23' || dateStr === '2021-12-27' || dateStr === '2021-12-28' || dateStr === '2021-12-31' || dateStr === '2022-01-03' )
     if (is23Dec)
     {
         results = TIME_SLOTS_10_14;
