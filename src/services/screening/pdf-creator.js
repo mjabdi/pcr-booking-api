a07e60baef379f8d2c86aa0b52ab36ba16111fb5
mjabdi <m.jafarabdi@gmail.com>
2023-04-14 15:25:45 +0330

    hs report v2.0

--- src/services/screening/pdf-creator.js
+++ src/services/screening/pdf-creator.js
@@ -18,70 +18,174 @@ const createScreeningReportPDF = async (booking, reportData, package) => {
 
     const doc = new PDFDocument();
 
-    doc.image('assets/pdf-logo.png', 0, 0,  {fit: [615, 800], align: 'center', valign: 'top'});
+    doc.image('assets/medex_report_banner.png', 0, 0,  {fit: [800, 533], align: 'center', valign: 'top'});
 
-    doc.fillColor('#105d8f').fontSize(12).font('Helvetica-Bold').text('THE LONDON CLINIC HEALTH SCREENING REPORT', 140, 100, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+    doc.image('assets/pdf-logo.png', 0, 450,  {fit: [615, 200], align: 'center', valign: 'bottom'});
+
+    doc.fillColor('#968075').fontSize(14).font('Helvetica-Bold').text("Health Screening Report for:", 300, 100, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+
+    doc.fillColor('#735f55').fontSize(20).font('Helvetica-Bold').text(booking.fullname, 300, 125, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
 
 
-    doc.moveTo(20, 120).lineTo(600, 120).dash(5, {space: 5}).strokeColor("#777").stroke() 
+    doc.addPage()
 
-    doc.fillColor('black').fontSize(10).font('Times-Roman').text('Name:', 20, 130, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
-    doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(booking.fullname, 70, 130, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
 
-    doc.fillColor('black').fontSize(10).font('Times-Roman').text('DOB:', 20, 145, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
-    doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(NormalizeDate(booking.birthDate), 70, 145, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
 
-    doc.moveTo(20, 160).lineTo(600, 160).dash(5, {space: 5}).stroke() 
+    // doc.fillColor('#105d8f').fontSize(12).font('Helvetica-Bold').text('THE LONDON CLINIC HEALTH SCREENING REPORT', 140, 100, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
 
-    let start_y = 180
+    doc.rect(20, 40, 550, 55);
+    doc.fill("#035382");
+
+    doc.fillColor('#f1f1f1').fontSize(10).font('Helvetica').text('Name:', 30, 55, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+    doc.fillColor('#fff').fontSize(10).font('Helvetica-Bold').text(booking.fullname, 70, 55, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+
+    doc.fillColor('#f1f1f1').fontSize(10).font('Helvetica').text('DOB:', 30, 75, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+    doc.fillColor('#fff').fontSize(10).font('Helvetica-Bold').text(NormalizeDate(booking.birthDate), 70, 75, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+
+
+    const introText = "Your tests have been compared to standard reference ranges to give you an indication of a normal health result. These were correct at the time of result generation but are subject to change in line with advanced medical practices and research."
+    doc.fillColor('#013257').fontSize(14).font('Helvetica-Bold').text("Understanding your results", 40, 130, {characterSpacing : 1, wordSpacing : 1 , align : 'left'} );
+    doc.fillColor('black').fontSize(12).font('Helvetica').text(introText, 40, 160 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 300, continued: false})
+
+
+    doc.rect(40, 320, 500, 350).dash(5, {space: 5}).strokeColor("#999").stroke()
+    doc.fillColor('#aaa').fontSize(20).font('Helvetica').text('Some text here...', 200, 480, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+
+    doc.rect(360, 120, 180, 180).dash(5, {space: 5}).strokeColor("#999").stroke()
+    doc.fillColor('#aaa').fontSize(16).font('Helvetica').text('image...', 420, 200, {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+
+
+
+    doc.addPage()
+
+    let start_y = 50
     const row_height = 300
     const page_height = 600
+    let index = 0
+    let counterPage = 0
     for (var i=0 ; i < reportData.length ; i++)
     {      
       if (reportData[i].value)
       {
         const {desc, low, high, inRange} = getDescriptionForItem(reportData[i].param, reportData[i].value, package)
-
-        if (reportData[i].value === "low")
+        if (index % 2 === 0)
         {
-          doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
-          doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("LOW", 20, start_y + 15 , {characterSpacing : 1, wordSpacing : 1 , align : 'left'} );
 
-          doc.fillColor('black').fontSize(10).font('Helvetica').text(desc, 20, start_y + 35 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 550, continued: true})
-            .fillColor('red').fontSize(10).font('Helvetica-Bold').text(low,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 550, continued: false})
+          doc.rect(10, 20, 270, 730);
+          doc.fill("#f5fafa");      
 
-        }else if (reportData[i].value === "high")
-        {
-          doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
-          doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("HIGH", 20, start_y + 15 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );        
+          doc.rect(10, 20, 270, 70);
+          doc.fill("#d3dbdb");      
 
-          doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, 20, start_y + 35 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 550, continued: true})
-            .fillColor('red').fontSize(10).font('Helvetica-Bold').text(high,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 550, continued: false})
+          const text_color = "#494f4f"
+
+          if (reportData[i].value === "low")
+          {
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("LOW", 20, start_y + 20 , {characterSpacing : 1, wordSpacing : 1 , align : 'left'} );
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica').text(desc, 20, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+              .fillColor('red').fontSize(10).font('Helvetica-Bold').text(low,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
 
 
+              doc.image('assets/warning.png', 20, 25,  {fit: [20, 20], align: 'left', valign: 'top'});  
+          }
+          else if (reportData[i].value === "high")
+          {
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("HIGH", 20, start_y + 20 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );        
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, 20, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+              .fillColor('red').fontSize(10).font('Helvetica-Bold').text(high,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
+  
+              doc.image('assets/warning.png', 20, 25,  {fit: [20, 20], align: 'left', valign: 'top'});  
 
-        }else if (reportData[i].value === "inRange")
-        {
-          doc.fillColor('black').fontSize(10).font('Times-Roman').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
-          doc.fillColor('green').fontSize(10).font('Helvetica-Bold').text("NORMAL", 20, start_y + 15 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+  
+          }else if (reportData[i].value === "inRange")
+          {
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, 20, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('green').fontSize(10).font('Helvetica-Bold').text("NORMAL", 20, start_y + 20 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, 20, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+            .fillColor('black').fontSize(10).font('Helvetica-Bold').text(inRange,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
 
-          doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, 20, start_y + 35 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 550, continued: true})
-          .fillColor('black').fontSize(10).font('Helvetica-Bold').text(inRange,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 550, continued: false})
+              doc.image('assets/tick.png', 20, 25,  {fit: [20, 20], align: 'left', valign: 'top'});
+
+  
+          }
+  
+
+        }else{
+          const start_x = 300
+
+          doc.rect(290, 20, 270, 730);
+          doc.fill("#fcf7fc");      
+
+          doc.rect(290, 20, 270, 70);
+          doc.fill("#e0d3e0");      
+         
+          const text_color = "#4d464d"
+          if (reportData[i].value === "low")
+          {
+      
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, start_x, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("LOW", start_x, start_y + 20 , {characterSpacing : 1, wordSpacing : 1 , align : 'left'} );
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica').text(desc, start_x, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+              .fillColor('red').fontSize(10).font('Helvetica-Bold').text(low,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
+  
+              doc.image('assets/warning.png', 300, 25,  {fit: [20, 20], align: 'left', valign: 'top'});  
+
+            }
+          else if (reportData[i].value === "high")
+          {
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, start_x, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('red').fontSize(10).font('Helvetica-Bold').text("HIGH", start_x, start_y + 20 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );        
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, start_x, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+              .fillColor('red').fontSize(10).font('Helvetica-Bold').text(high,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
+  
+              doc.image('assets/warning.png', 300, 25,  {fit: [20, 20], align: 'left', valign: 'top'});  
+  
+  
+          }else if (reportData[i].value === "inRange")
+          {
+            doc.fillColor(text_color).fontSize(10).font('Helvetica-Bold').text(reportData[i].param, start_x, start_y , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+            doc.fillColor('green').fontSize(10).font('Helvetica-Bold').text("NORMAL", start_x, start_y + 20 , {characterSpacing : 1, wordSpacing : 1.2 , align : 'left'} );
+  
+            doc.fillColor('black').fontSize(10).font('Helvetica-Bold').text(desc, start_x, start_y + 70 , {lineGap:7 ,characterSpacing : 1, wordSpacing : 1.2 , align : 'justify', width: 250, continued: true})
+            .fillColor('black').fontSize(10).font('Helvetica-Bold').text(inRange,  {lineGap:7, characterSpacing : 1, wordSpacing : 1 , align : 'justify', width: 250, continued: false})
+
+            doc.image('assets/tick.png', 300, 25,  {fit: [20, 20], align: 'left', valign: 'top'});  
+
+  
+          }
+
+
 
         }
 
+        index++;
 
-        if (i > 0)
+        counterPage++
+        if (counterPage >= 2)
         {
-          doc.moveTo(20, start_y - 10).lineTo(600, start_y  - 10).dash(5, {space: 5}).stroke() 
-        }
-
-        start_y += row_height
-        if (start_y > page_height)
-        {
-          start_y = 50
+          counterPage = 0
           doc.addPage()
         }
+
+
+        // if (i > 0)
+        // {
+        //   doc.moveTo(20, start_y - 10).lineTo(600, start_y  - 10).dash(5, {space: 5}).stroke() 
+        // }
+
+        // start_y += row_height
+        // if (start_y > page_height)
+        // {
+        //   start_y = 50
+        //   doc.addPage()
+        // }
       }
     }
 
