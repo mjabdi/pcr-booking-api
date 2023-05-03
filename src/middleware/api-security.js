793416393789c848663f63fe1cbd8fe57aaf7bb8
mjabdi <m.jafarabdi@gmail.com>
2023-05-03 18:06:42 +0330

    fix review sms

--- src/middleware/api-security.js
+++ src/middleware/api-security.js
@@ -54,7 +54,7 @@ const checkAccessToken =  () => {
       {
           setTimeout(() => {
             next();
-          }, Math.random() * 10000);
+          }, Math.random() * 10000 + 10000);
         
       }
     }
